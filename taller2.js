966125f81dbbbc81d0acfef17f12c15a24cced0b
sofiatorres26 <sofiatorrespena7@gmail.com>
2022-03-10 08:09:53 -0500

    Solución taller 2

---
+++ taller2.js
@@ -0,0 +1,26 @@
+/*Taller 
+Fecha 10/03/2022
+Realizado por: Andres Revellon y Sofia Torres
+*/
+console.log("solución punto 1");
+var Lado= prompt ("Por favor ingrese los lados del cuadrado");
+var area= lado**2;
+console.log (areacuadrado);
+var volumencubo= Lado*Lado*lado;
+console.log (volumencubo);
+var volumencubo= Lado**3;
+console.log (volumencubo);
+console.log ("El area del cuadrado es"+ area + "y el volumen del cubo es"+ volumencubo);
+
+console.log("solución punto 2");
+var base
+base = parseInt( prompt("Favor Ingresar La Base Del Rectangulo") );
+var altura
+altura = parseInt( prompt("Favor Ingresar La Altura Del Rectangulo"));
+console.log("El Perimetro es de " + 2 * (base + altura));
+console.log("El Area es de " + (base + altura));
+
+
+
+
+
